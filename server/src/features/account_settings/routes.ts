94f05f03cf69fb2e0be48e053accc5363be32c92
ArtificialLegacy <redsbro457@gmail.com>
2023-12-03 21:06:28 -0500

    move edit route into it's own function

--- server/src/features/account_settings/routes.ts
+++ server/src/features/account_settings/routes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express'
 
-import { sessionGetUser, authUserCheck } from 'features/authentication'
-import GenericResult from 'types/generic_result'
-import updateUserSettings from './services/update_user_settings'
+import { authUserCheck } from 'features/authentication'
+import editRoute from './routes/edit'
 
 /**
  * @middleware authUserCheck
@@ -15,29 +14,6 @@ const router = Router()
 
 router.use(authUserCheck)
 
-router.put('/edit', async (_req, _res) => {
-  const user = await sessionGetUser(_res.locals.cookies?.session)
-  if (user === undefined || user === null) {
-    _res.status(403)
-    _res.send({ status: GenericResult.Failed })
-    return
-  }
-
-  if (_req.body.theme < 0 && _req.body.theme > 2) {
-    _res.status(400)
-    _res.send({ status: GenericResult.InvalidRequest })
-    return
-  }
-
-  const success = await updateUserSettings(user, _req.body.theme)
-
-  if (!success) {
-    _res.status(500)
-    _res.send({ status: GenericResult.Failed })
-  }
-
-  _res.status(200)
-  _res.send({ status: GenericResult.Success })
-})
+router.put('/edit', editRoute)
 
 export default router
