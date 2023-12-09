67e685fafa86dc7f65ed0d22526b33b0786cdb6c
ArtificialLegacy <redsbro457@gmail.com>
2023-12-09 08:28:40 -0500

    move song stream route to it's own file

--- server/src/features/song_stream/routes.ts
+++ server/src/features/song_stream/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-import fs from 'fs'
+
+import stream from './routes/stream'
 
 /**
  * @endpoint '/:filepath' - Endpoint to stream an audio file to the client.
@@ -8,30 +9,6 @@ import fs from 'fs'
  */
 const router = Router()
 
-router.get('/:filepath', (_req, _res) => {
-  if (_req.params.filepath.length < 0 || _req.params.filepath.length > 36)
-    _res.sendStatus(400)
-
-  const path = `public/songs/${_req.params.filepath}.m4a`
-
-  const stream = fs.createReadStream(path)
-  const stat = fs.statSync(path)
-
-  _res.set('Content-Type', 'audio/x-m4a')
-  _res.set('Accept-Ranges', 'bytes')
-  _res.set('Content-Length', '' + stat.size)
-
-  stream.on('data', (_chunk) => {
-    _res.write(_chunk)
-  })
-
-  stream.on('error', () => {
-    _res.sendStatus(404)
-  })
-
-  stream.on('end', () => {
-    _res.end()
-  })
-})
+router.get('/:filepath', stream)
 
 export default router
---
+++ server/src/features/song_stream/routes/stream.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express'
+import fs from 'fs'
+
+async function stream(_req: Request, _res: Response) {
+  if (_req.params.filepath.length < 0 || _req.params.filepath.length > 36)
+    _res.sendStatus(400)
+
+  const path = `public/songs/${_req.params.filepath}.m4a`
+
+  const stream = fs.createReadStream(path)
+  const stat = fs.statSync(path)
+
+  _res.set('Content-Type', 'audio/x-m4a')
+  _res.set('Accept-Ranges', 'bytes')
+  _res.set('Content-Length', '' + stat.size)
+
+  stream.on('data', (_chunk) => {
+    _res.write(_chunk)
+  })
+
+  stream.on('error', () => {
+    _res.sendStatus(404)
+  })
+
+  stream.on('end', () => {
+    _res.end()
+  })
+}
+
+export default stream
